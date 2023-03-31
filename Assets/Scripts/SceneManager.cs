95f9e80059edf1342943b0560428239a841abbc1
Nikawak <nikawak228@gmail.com>
2023-03-31 14:16:11 +0300

    sounds, mainMenu and general scene added

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Nikita2");
+    }
+}
