a79fe90a80a3d003ea38639d56ff5be559c9797d
who <nadezhda.bn.02@mail.ru>
2023-04-07 00:50:04 +0300

    Create Inventory include change weapons, add not working change ui-indicator of current weapon. Base Player, Character scripts

--- Assets/Prefabs/Enemy/Enemy.cs
+++ Assets/Prefabs/Enemy/Enemy.cs
@@ -1,24 +1,18 @@
+using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
     public List<Transform> Path;
     public EnemyVision enemyVision;
-    
-    [SerializeField] private UnityEvent OnDeath;
-
-    [Min(0)]
-    [SerializeField] private int Health;
-
-
-    public int TakeDamage(int damage)
+    public float TakeDamage(float damage)
     {
-        Health -= damage;
         if (Health < 0)
             OnDeath.Invoke();
+        Health -= damage;
         return Health;
     }
 }
