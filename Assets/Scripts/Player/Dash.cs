beb5cfd91d430cd447145024ef391bd7759b1aa6
Nikawak <nikawak228@gmail.com>
2023-04-17 22:15:46 +0300

    vadim2 playable scene

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -1,9 +1,6 @@
-using OpenCover.Framework.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Dash : MonoBehaviour
